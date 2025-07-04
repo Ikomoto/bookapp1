bc4ed01a1242f86826dcdac9c4d33afd6b7ff238
kazami <shota-234@outlook.jp>
2025-07-04 19:38:02 +0900

    20250704-desktop

--- src/componets/BookList.js
+++ src/componets/BookList.js
@@ -27,6 +27,7 @@ function BookList(){
                 <img src={book.image} alt={book.title} />
                 <h4>{book.title}</h4>
                 <p>{book.author}</p>
+                <p>{book.description}</p>
             </div>
         ))
         }
