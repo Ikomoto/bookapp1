dda81f1d658843d97c76f4563e25b08983c39418
kazami <shota-234@outlook.jp>
2025-07-07 15:29:50 +0900

    20250707_一覧表示追加

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Pokemon from "./componets/Pokemon";
 import {getAllPokemon} from "./componets/Pokemon";
 import PokemonDetail from "./componets/PokemonDetail";
 
+
 const FetchJapaneseNames = async () => {
     const results =[];
     for(let id=1; id<= 151; id++){
@@ -15,20 +16,52 @@ const FetchJapaneseNames = async () => {
         const data = await res.json();
         const jaNameData = data.names.find(n => n.language.name === "ja");
         const enNameData = data.names.find(n => n.language.name === "en");
-        //const jaName = jaNameData.name;
-        //console.log(enNameData);
+
         results.push({
             id: id,
             jaName: jaNameData?.name || "不明",
             enName: enNameData?.name|| "不明"
         });
     }
-    console.log(results);
+    //console.log(results);
     return results;
+};
+
+
+const GetPokemonList = async () => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+    const data = await res.json();
+    return data;
+    //console.log(data.results);
+};
+
+const GetPokemonDetailList = async (urlList) => {
+    let pokemonData = Promise.all(
+        urlList.map((pokemon) => {
+            let pokemonRecord = getPokemon(pokemon.url);
+            return pokemonRecord;
+        })
+    );
+    return pokemonData;
+};
+
+const getPokemon = async(url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                //console.log(data)
+                resolve(data);
+            });
+    })
+    
 };
 
 function App() {
     const [pokeName_ja_en, setPokeName_ja_en] = useState([]);
+    //const [pokemonList, setPokemonList] = useState([]);
+    const [pokemonDetail, setPokemonDetail] = useState([]);
+    const [loading, setLoading] = useState(true );
 
     useEffect(() => {
         const GetData = async () => {
@@ -37,19 +70,33 @@ function App() {
         }
         GetData();
     },[]);
-    console.log(pokeName_ja_en);
 
-  return (
-    <Router>
-      <Routes>
-          <Route path="/" element={<Pokemon pokemonInfo={pokeName_ja_en}/>}></Route>
-          <Route path="/booklist" element={<BookList />}></Route>
-          <Route path="/bookdetail" element={<BookDetail />}></Route>
-          <Route path="/pokemon" element={<Pokemon />}></Route>
-          <Route path="/pokemon/:id" element={<PokemonDetail pokemonInfo={pokeName_ja_en}/>}></Route>
-      </Routes>
-    </Router>
-  );
+    useEffect(() => {
+        const GetPokemonListData = async () => {
+            const data = await GetPokemonList();
+            const detail = await GetPokemonDetailList(data.results); 
+            //console.log(detail);
+            setLoading(false);
+            setPokemonDetail(detail);
+        };
+        GetPokemonListData();
+    },[]);
+
+
+
+    //console.log(pokemonDetail);
+
+    return (
+        <Router>
+        <Routes>
+            <Route path="/" element={<Pokemon pokemonList={pokemonDetail} loading={loading}/>}></Route>
+            <Route path="/booklist" element={<BookList />}></Route>
+            <Route path="/bookdetail" element={<BookDetail />}></Route>
+            <Route path="/pokemon" element={<Pokemon />}></Route>
+            <Route path="/pokemon/:id" element={<PokemonDetail pokemonInfo={pokeName_ja_en}/>}></Route>
+        </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- src/componets/Pokemon.js
+++ src/componets/Pokemon.js
@@ -3,65 +3,39 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PokemonDetail from "./PokemonDetail";
 import { Link } from "react-router-dom";
+import Card from "./Cards/Card.js"
+import './pokemon.css';
 
-/*
-
-const FetchJapaneseNames = async () => {
-    const results =[];
-    for(let id=1; id<= 151; id++){
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`);
-        const data = await res.json();
-        const jaNameData = data.names.find(n => n.language.name === "ja");
-        const enNameData = data.names.find(n => n.language.name === "en");
-        //const jaName = jaNameData.name;
-        //console.log(enNameData);
-        results.push({
-            id: id,
-            jaName: jaNameData?.name || "不明",
-            enName: enNameData?.name|| "不明"
-        });
-    }
-    console.log(results);
-    return results;
-};
-*/
-
-function Pokemon({pokemonInfo}){
-    //const initialURL = "https://pokeapi.co/api/v2/pokemon";
-    //const initialURL2 = "https://pokeapi.co/api/v2/pokemon-species"
-    /*
-    const [pokeName_ja_en, setPokeName_ja_en] = useState([]);
-
-    useEffect(() => {
-        const GetData = async () => {
-            const names = await FetchJapaneseNames();
-            setPokeName_ja_en(names);
-        }
-        GetData();
-    },[]);
-
-    //console.log(pokeName_ja_en);
-    */
+function Pokemon({pokemonList, loading}){
 
     return (
-        <div>
-            <h2>ポケモンの名前一覧2</h2>
-            <ul>
-                {pokemonInfo.map((pokemon) => (
-                    <li key={pokemon.id}>
-                        <Link to={`/pokemon/${pokemon.id}`}>
-                            {pokemon.id}: {pokemon.jaName}: {pokemon.enName}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <ul>
-                {pokemonInfo.map((pokemon) => (
-                    <li key={pokemon.id}>
-                        {pokemon.id}: {pokemon.jaName}: {pokemon.enName}
-                    </li>
-                ))}
-            </ul>
+        <div className="App">
+            {loading ? (
+                <h1>ロード中・・・</h1>
+        ) : (
+            <>
+                <h2>ポケモンの名前一覧</h2>
+                
+                <div className="pokemonCardContainer">
+                    {pokemonList.map((pokemon, i) => {
+                        return <Card key={i} pokemon={pokemon}/>;
+                    })}
+                </div>
+                {/*
+                <ul>
+                    {pokemonList.map((pokemon) => (
+                        <li key={pokemon.id}>
+                            
+                            <Link to={`/pokemon/${pokemon.id}`}>
+                                {pokemon.id}: {pokemon.name}:
+                                <img src={pokemon.sprites.front_default} alt=""/>
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+                */}
+            </>
+            )}
         </div>
     );
 };
